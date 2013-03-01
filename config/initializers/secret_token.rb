47c00bd3c0848583ef10ee2df23891e10f57b264
rajofchennai <rajofchennai@yahoo.com>
2013-03-01 21:27:56 +0530

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApigeeHacathon::Application.config.secret_token = '88c92d56f2d48bb8b39b023523e02b024f0695137f221267de6945416725d5be798e5a7f6ac6f186191d60f58876a207cabc2260ed934386af7cceedd9b38469'
