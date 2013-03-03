a866ffab90aea0f1bab1608b839bf362286c8e66
Rutika <rutika@altheasystems.com>
2013-03-03 10:28:29 +0530

    Bug fix in phone number to send sms

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 
 MESSAGE_QUEUE = GirlFriday::WorkQueue.new(:send_sms, :size => 3) do |msg|
   puts "#{msg.inspect}"
-  url = "http://www.kookoo.in/outbound/outbound_sms.php?phone_no=#{msg[:phone_no]}&api_key=KKb05fc6024cdea32d87020f4ccffc85a5&message=#{msg[:message]}"
+  url = "http://www.kookoo.in/outbound/outbound_sms.php?phone_no=#{'0' + msg[:phone_no][-10, 10]}&api_key=KKb05fc6024cdea32d87020f4ccffc85a5&message=#{msg[:message]}"
 
   resp = RestClient.get(url)
   puts resp.inspect
